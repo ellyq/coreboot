497476f6a4cc3dd28994796bd5287143687d068b
Alicja Michalska <ahplka19@gmail.com>
2023-05-28 00:30:53 +0200

    mb/erying/tgl: GPIO fixup, rewrite romstage
    Signed-off-by: Alicja Michalska <ahplka19@gmail.com>

--- src/mainboard/erying/tgl/romstage.c
+++ src/mainboard/erying/tgl/romstage.c
@@ -5,39 +5,42 @@
 #include <spd_bin.h>
 #include "gpio.h"
 
-static const struct mb_cfg board_cfg = {
-        .type = MEM_TYPE_DDR4,
-        .ddr4_config = {
-                .dq_pins_interleaved = true,
-        },
-};
-
-static const struct mem_spd spd_info = {
-        .topo = MEM_TOPO_DIMM_MODULE,
-        .smbus = {
-                [0] = { .addr_dimm[0] = 0x50, },
-                [1] = { .addr_dimm[0] = 0x52, },
-        },
-};
-
 void mainboard_memory_init_params(FSPM_UPD *mupd)
 {
 
-        const bool half_populated = false;
+	static const struct mb_cfg mem_type = { 
+		.type = MEM_TYPE_DDR4,
+		.ddr4_config = { .dq_pins_interleaved = true },
+	};
+	static const struct mem_spd spd_info = {
+        	.topo = MEM_TOPO_DIMM_MODULE,
+		.smbus = {
+			[0] = { .addr_dimm[0] = 0x50, },
+			[1] = { .addr_dimm[0] = 0x52, },
+		},
+	};
+	const bool half_populated = false;
+        memcfg_init(mupd, &mem_type, &spd_info, half_populated);
 
-        memcfg_init(mupd, &board_cfg, &spd_info, half_populated);
+	/* FSP Configuration */
+	mupd->FspmConfig.UserBd = 1;
+	mupd->FspmConfig.PchHdaEnable = 1;
+	mupd->FspmConfig.SmbusEnable = 1;
+	mupd->FspmConfig.MemorySpdDataLen = 0x200;
 
-	// Performance settings //
+
+	/* Performance settings */
 	mupd->FspmConfig.VtdDisable = 0;
 	mupd->FspmConfig.VmxEnable = 1;
 	mupd->FspmConfig.HyperThreading = 1;
 	mupd->FspmConfig.OcSupport = 1;
+	mupd->FspmConfig.EnableAbove4GBMmio = 1;
 
 	/* XMP Configuration - selects XMP Profile 1 by default with 1.35v and CLKREF 100MHz.
 		To change those settings, refer to:
 	  	3rdparty/fsp/TigerLakeFspBinPkg/Client/Include/FspmUpd.h */
 
-	// Not functional yet! //
+	// Not functional yet! - Will result in FspNotify error 0x80000007! //
 	// mupd->FspmConfig.SpdProfileSelected = 2;
 	mupd->FspmConfig.RefClk = 1;
 	mupd->FspmConfig.VddVoltage = 1350;
