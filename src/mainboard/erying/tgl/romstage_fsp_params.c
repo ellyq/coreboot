ca078aed17034ed7f6148959804662ee1f2e5743
Alicja Michalska <ahplka19@gmail.com>
2024-01-09 03:47:50 +0100

    mb/erying/tgl: Change memory training behaviour
    Configure FSP in (mostly) the same way as stock firmware does.
    This should help with stability.
    
    Signed-off-by: Alicja Michalska <ahplka19@gmail.com>

--- src/mainboard/erying/tgl/romstage_fsp_params.c
+++ src/mainboard/erying/tgl/romstage_fsp_params.c
@@ -36,33 +36,56 @@ void mainboard_memory_init_params(FSPM_UPD *mupd)
         mupd->FspmConfig.DmiAspm = 0;
         mupd->FspmConfig.DmiAspmCtrl = 0;
 
+	// Memory training - based on stock
+	mupd->FspmConfig.ECT = 1;
+	mupd->FspmConfig.LCT = 1;
+	mupd->FspmConfig.SOT = 1;
+	mupd->FspmConfig.ERDMPRTC2D = 0;
+	mupd->FspmConfig.RDMPRT = 1;
+	mupd->FspmConfig.RCVET = 1;
+	mupd->FspmConfig.JWRL = 1;
+	mupd->FspmConfig.EWRTC2D = 1;
+	mupd->FspmConfig.ERDTC2D = 1;
+	mupd->FspmConfig.WRTC2D = 1;
+	mupd->FspmConfig.WRVC1D = 1;
+	mupd->FspmConfig.WRVC1D = 1;
+	mupd->FspmConfig.DIMMODTT = 1;
+	mupd->FspmConfig.DIMMRONT = 1;
+	mupd->FspmConfig.WRDSEQT = 1;
+	mupd->FspmConfig.WRSRT = 0;
+	mupd->FspmConfig.RDODTT = 1;
+	mupd->FspmConfig.RDEQT = 1;
+	mupd->FspmConfig.RDAPT = 1;
+	mupd->FspmConfig.RDTC2D = 1;
+	mupd->FspmConfig.WRVC2D = 1;
+	mupd->FspmConfig.RDVC2D = 1;
+	mupd->FspmConfig.CMDVC = 1;
+	mupd->FspmConfig.MrcSafeConfig = 0;
+	mupd->FspmConfig.LpDdrDqDqsReTraining = 1;
+	mupd->FspmConfig.SafeMode = 0;
+	mupd->FspmConfig.OverrideDowngradeForMixedMemory = 0;
+	mupd->FspmConfig.MemTestOnWarmBoot = 1;
+	// mupd->FspmConfig.DdrFreqLimit = 18; - Undocumented. Guessing 18 will be 3200MHz. Disabling for now.
+	mupd->FspmConfig.SaGv = 0;
+	// mupd->FspmConfig.RetrainOnFastFail = 1; - Commenting out for now, can't find an equivalent in FSP-M headers.
+	mupd->FspmConfig.NModeSupport = 0; // Board type is 1N (1DPC). Shoud be set to "1", testing auto "0" for now.
+	mupd->FspmConfig.RhPrevention = 0;
+	mupd->FspmConfig.RefreshWm = 1;
+	mupd->FspmConfig.ExitOnFailure = 1;
+
 	// Performance settings
-	mupd->FspmConfig.HyperThreading = 1;
-	mupd->FspmConfig.EnableAbove4GBMmio = 1;
-	mupd->FspmConfig.OcSupport = 1;
-	mupd->FspmConfig.OcLock = 0;
-	mupd->FspmConfig.SaOcSupport = 1;
-
-	// Memory configuration
-	mupd->FspmConfig.SpdProfileSelected = 2; // XMP Profile 1
-	mupd->FspmConfig.DdrFreqLimit = 3200;
-
-	mupd->FspmConfig.SaGv = 4;
-	mupd->FspmConfig.SaGvFreq[0] = 2133;
-	mupd->FspmConfig.SaGvGear[0] = 1;
-	mupd->FspmConfig.SaGvFreq[1] = 2400;
-	mupd->FspmConfig.SaGvGear[1] = 2;
-	mupd->FspmConfig.SaGvFreq[2] = 3200;
-	mupd->FspmConfig.SaGvGear[2] = 1;
-	mupd->FspmConfig.SaGvFreq[3] = 3200;
-	mupd->FspmConfig.SaGvGear[3] = 2;
-
+	mupd->FspmConfig.SpdProfileSelected = 3; // XMP Profile 2 - platform is unstable with memory above 2933MHz.
 	mupd->FspmConfig.RefClk = 1; // 100MHz
-	mupd->FspmConfig.VddVoltage = 1350; // 1.35v
+	mupd->FspmConfig.VddVoltage = 1200; // 1.2V
 	mupd->FspmConfig.Ratio = 0;
-	mupd->FspmConfig.RingDownBin = 1;
-	mupd->FspmConfig.GearRatio = 2;
-	mupd->FspmConfig.NModeSupport = 1; // Board type is 1N
+	mupd->FspmConfig.RingDownBin = 0;
+	mupd->FspmConfig.GearRatio = 0;
+
+        mupd->FspmConfig.HyperThreading = 1;
+        mupd->FspmConfig.EnableAbove4GBMmio = 1;
+        mupd->FspmConfig.OcSupport = 1;
+        mupd->FspmConfig.OcLock = 0;
+        mupd->FspmConfig.SaOcSupport = 1;
 
 	memcfg_init(mupd, &mem_config, &spd_info, half_populated);
 	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
